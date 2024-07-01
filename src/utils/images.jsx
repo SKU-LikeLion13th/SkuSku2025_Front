afbd3af3e2f18beebd9c7d058d1aaed8dcf6e947
seung-heee <tmdgmldi2595@naver.com>
2024-07-01 10:35:23 +0900

    feat: track intro

---
+++ src/utils/images.jsx
@@ -0,0 +1,10 @@
+const PATH = process.env.PUBLIC_URL;
+
+export const images = {
+    track_frontend_background : PATH + 'assets/images/track/frontend_background.png',
+    track_backend_background : PATH + 'assets/images/track/backend_background.png',
+    track_pm_background : PATH + 'assets/images/track/pm_background.png',
+    track_frontend_curri : PATH + 'assets/images/track/frontend_curri.png',
+    track_backend_curri : PATH + 'assets/images/track/backend_curri.png',
+    track_pm_curri : PATH + 'assets/images/track/pm_curri.png',
+};
