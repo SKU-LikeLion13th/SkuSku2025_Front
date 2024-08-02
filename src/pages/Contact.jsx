a08f3b3149124653cde5c62aa64910d088fd539c
zerohyun <kyhh39@naver.com>
2024-08-03 00:41:20 +0900

    design : 반응형 구현중

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -4,14 +4,14 @@ import { useLocation } from 'react-router-dom';
 
 const Contact = () => {
   return (
-    <div className="flex flex-col justify-center items-center h-screen bg-black">
-      <div className="flex flex-row justify-between items-center w-full px-20">
-        <div className="w-[610px] mr-20">
-          <span className="text-blue-500 text-6xl fontEB leading-tight">
+    <div className="flex flex-col justify-center items-center min-h-screen bg-black">
+      <div className="flex flex-col lg:flex-row justify-between items-center w-full px-6 lg:px-20">
+        <div className="w-full lg:w-[610px] mb-10 lg:mb-0 lg:mr-20">
+          <span className="text-blue-500 text-4xl lg:text-6xl fontEB leading-tight">
             반가워요!
             <br />
           </span>
-          <span className="text-white text-6xl fontEB leading-tight">
+          <span className="text-white text-4xl lg:text-6xl fontEB leading-tight">
             어떤 것을
             <br />
             도와드릴까요?
@@ -28,15 +28,15 @@ const ContactTabs = () => {
   const [selectedTab, setSelectedTab] = useState('etc');
 
   useEffect(() => {
-    const tab = location.search.replace('?', '')
+    const tab = location.search.replace('?', '');
     setSelectedTab(tab);
-  }, [location])
+  }, [location]);
 
   return (
-    <div className="flex flex-row justify-center items-center space-x-10 w-full h-64 ml-40 mb-20 relative">
-      <div className="flex flex-col space-y-28 w-1/5 ml-10">
+    <div className="flex flex-col lg:flex-row justify-center items-center space-y-10 lg:space-y-0 lg:space-x-10 w-full lg:h-64 mb-10 lg:mb-20 relative">
+      <div className="flex flex-col space-y-10 lg:space-y-28 w-full lg:w-1/5">
         <button
-          className={`text-3xl fontMedium flex items-center relative ${
+          className={`text-xl lg:text-3xl fontMedium flex items-center relative ${
             selectedTab === 'etc' ? 'text-white' : 'text-gray-500'
           }`}
           onClick={() => setSelectedTab('etc')}>
@@ -45,12 +45,12 @@ const ContactTabs = () => {
             <img
               src={images.arrow}
               alt="arrow"
-              className={`w-full mt-8 ${selectedTab === 'etc' ? 'opacity-100' : 'opacity-50'}`}
+              className={`w-12 h-6 lg:w-auto lg:h-auto mt-8 ${selectedTab === 'etc' ? 'opacity-100' : 'opacity-50'}`}
             />
           </span>
         </button>
         <button
-          className={`text-3xl fontMedium flex items-center relative ${
+          className={`text-xl lg:text-3xl fontMedium flex items-center relative ${
             selectedTab === 'Collaboration' ? 'text-white' : 'text-gray-500'
           }`}
           onClick={() => setSelectedTab('Collaboration')}>
@@ -59,12 +59,14 @@ const ContactTabs = () => {
             <img
               src={images.arrow}
               alt="arrow"
-              className={`w-full mt-8 ${selectedTab === 'Collaboration' ? 'opacity-100' : 'opacity-50'}`}
+              className={`w-12 h-6 lg:w-auto lg:h-auto mt-8 ${
+                selectedTab === 'Collaboration' ? 'opacity-100' : 'opacity-50'
+              }`}
             />
           </span>
         </button>
         <button
-          className={`text-3xl fontMedium flex items-center relative ${
+          className={`text-xl lg:text-3xl fontMedium flex items-center relative ${
             selectedTab === 'inquiry' ? 'text-white' : 'text-gray-500'
           }`}
           onClick={() => setSelectedTab('inquiry')}>
@@ -73,16 +75,18 @@ const ContactTabs = () => {
             <img
               src={images.arrow}
               alt="arrow"
-              className={`w-full mt-8 ${selectedTab === 'inquiry' ? 'opacity-100' : 'opacity-50'}`}
+              className={`w-12 h-6 lg:w-auto lg:h-auto mt-8 ${
+                selectedTab === 'inquiry' ? 'opacity-100' : 'opacity-50'
+              }`}
             />
           </span>
         </button>
       </div>
-      <div className="relative flex-grow flex items-center justify-center h-full -ml-10">
+      <div className="relative flex-grow flex items-center justify-center h-full">
         {selectedTab === 'etc' && (
-          <div className="absolute top-0 bg-[#1C58DA] text-white p-4 rounded-lg w-72 h-44 flex fontLight text-xl">
+          <div className="absolute top-0 bg-[#1C58DA] text-white p-4 rounded-lg w-full lg:w-72 h-44 flex fontLight text-base lg:text-xl">
             <div>
-              <span className="fontSB text-xl">기타 의뢰</span>는 카카오톡 채널 <br />
+              <span className="fontSB text-lg lg:text-xl">기타 의뢰</span>는 카카오톡 채널 <br />
               "멋쟁이사자처럼 at 성결대"
               <br />로 문의 바랍니다.
               <br />
@@ -92,9 +96,10 @@ const ContactTabs = () => {
           </div>
         )}
         {selectedTab === 'Collaboration' && (
-          <div className="absolute top-24 bg-[#34C242] text-white p-4 rounded-lg w-72 h-44 flex items-center justify-center fontLight text-xl">
+          <div className="absolute top-24 bg-[#34C242] text-white p-4 rounded-lg w-full lg:w-72 h-44 flex items-center justify-center fontLight text-base lg:text-xl">
             <div>
-              <span className="fontSB text-xl">협업 문의</span>는 "sungkyul.univ@likelion.org" 위 메일 주소로 문의 바랍니다.
+              <span className="fontSB text-lg lg:text-xl">협업 문의</span>는 "sungkyul.univ@likelion.org" 위 메일 주소로
+              문의 바랍니다.
               <br />
               <br />
               감사합니다!
@@ -102,9 +107,10 @@ const ContactTabs = () => {
           </div>
         )}
         {selectedTab === 'inquiry' && (
-          <div className="absolute top-60 bg-[#747474] text-white p-4 rounded-lg w-72 h-44 flex items-center justify-center text-xl fontLight">
+          <div className="absolute top-48 bg-[#747474] text-white p-4 rounded-lg w-full lg:w-72 h-44 flex items-center justify-center text-base lg:text-xl fontLight">
             <div>
-              <span className="fontSB text-xl">문의 사항</span>은 인스타그램 @likelion_sku DM으로 문의 바랍니다.
+              <span className="fontSB text-lg lg:text-xl">문의 사항</span>은 인스타그램 @likelion_sku DM으로 문의
+              바랍니다.
               <br />
               <br />
               감사합니다!
