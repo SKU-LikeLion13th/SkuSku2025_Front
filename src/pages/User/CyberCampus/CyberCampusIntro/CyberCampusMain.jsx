513f81342273b761787a67176011add5b9090c0b
seung-heee <tmdgmldi2595@naver.com>
2024-08-09 23:39:25 +0900

    feat: 사이버캠퍼스 메인 > 파트별 경로 설정

--- src/pages/User/CyberCampus/CyberCampusIntro/CyberCampusMain.jsx
+++ src/pages/User/CyberCampus/CyberCampusIntro/CyberCampusMain.jsx
@@ -1,26 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { images } from '../../../../utils/images';
 import { useNavigate } from 'react-router-dom';
+import { IoIosArrowBack } from "react-icons/io";
 
 const CyberCampusMain = () => {
+  const [trackItemIn, setTrackItemIn] = useState(false);
+  const [selectedSubtitle, setSelectedSubtitle] = useState(null);
+
+  const handleTrackClick = (subtitle) => {
+    setTrackItemIn(prev => !prev);
+    setSelectedSubtitle(subtitle);
+  };
+
   const trackData = [
     {
       title: '프론트엔드',
       subtitle: 'FRONT-END',
       bgColor: 'bg-gradient-to-r from-[#FFE3CF] to-[#FF8C3A]',
-      navigateTo: '#',
+      navigateTo: 'front-end',
     },
     {
       title: '백엔드',
       subtitle: 'BACK-END',
       bgColor: 'bg-gradient-to-r from-[#BDFFFF] to-[#47EAEA]',
-      navigateTo: '#',
+      navigateTo: 'back-end',
     },
     {
       title: '기획/디자인',
       subtitle: 'PM/DESIGN',
       bgColor: 'bg-gradient-to-r from-[#FFD1E2] to-[#FF87B1]',
-      navigateTo: '#',
+      navigateTo: 'pmDesign',
     },
   ];
 
@@ -39,33 +48,36 @@ const CyberCampusMain = () => {
       <div className='py-12'>성결대학교 아기사자들을 위한 학습 공간입니다.</div>
 
       {/* Track Box */}
+      {!trackItemIn && 
       <div className="container mx-auto xl:w-full xl:px-0">
-      <div className="grid w-full grid-cols-1 gap-8 xl:grid-cols-3 ">
-        {trackData.map((track, index) => (
+        <div className="grid w-full grid-cols-1 gap-8 xl:grid-cols-3 ">
+          {trackData.map((track, index) => (
           <TrackItem
-            key={index}
-            title={track.title}
-            subtitle={track.subtitle}
-            bgColor={track.bgColor}
-            navigateTo={track.navigateTo}
+              key={index}
+              title={track.title}
+              subtitle={track.subtitle}
+              bgColor={track.bgColor}
+              navigateTo={track.navigateTo}
+              handleTrackClick={handleTrackClick}
           />
-        ))}
-      </div>
-    </div>
+          ))}
+        </div>
+      </div>}
+
+      {trackItemIn && <TrackItemIn title={selectedSubtitle} handleTrackClick={handleTrackClick} /> }
     </div>
   )
 };
 
 export default CyberCampusMain;
 
-
-const TrackItem = ({ title, subtitle, bgColor, navigateTo }) => {
+const TrackItem = ({ title, subtitle, bgColor, navigateTo, handleTrackClick}) => {
   const navigate = useNavigate();
 
   return (
     <div className="p-4">
       <button
-        onClick={() => navigate(navigateTo)}
+        onClick={() => handleTrackClick(subtitle)}
         className={`relative group flex flex-col justify-between mx-auto w-4/5 xl:w-full h-[14rem] text-black rounded-md ${bgColor} transition-colors duration-300`}>
         <div className="flex justify-between w-full h-full">
           <div className="pb-5 pl-8 text-start">
@@ -75,11 +87,64 @@ const TrackItem = ({ title, subtitle, bgColor, navigateTo }) => {
             </div>
           </div>
           <div className="flex items-end justify-end h-full">
-            {/* <img src={images.arrowM} alt="화살표" className="h-[14%] mb-6 mr-5 z-10" /> */}
             <img src={images.arrowB} alt="화살표" className="h-[14%] mb-6 mr-5 z-10" />
           </div>
         </div>
       </button>
     </div>
+  );
+};
+
+const TrackItemIn = ({ navigateTo, title, handleTrackClick }) => {
+  const navigate = useNavigate();
+
+  return (
+    <div className='flex'>
+    <button onClick={handleTrackClick}>
+      <IoIosArrowBack size={40}/>
+    </button>
+    
+    <div className="container mx-auto xl:w-full xl:px-0">
+      <div className="grid w-full grid-cols-1 gap-8 xl:grid-cols-2">
+        {/* 강의자료 */}
+        <div className="p-4">
+        <button
+          id={title}
+          onClick={() => navigate(`/cyberCampus/lecture/${title}`)}
+          className={`mx-auto relative group flex flex-col justify-between w-4/5 xl:w- h-[14rem] text-black rounded-md transition-colors duration-300 bg-[#3B79FF]`}>
+          <div className="flex justify-between w-full h-full">
+            <div className="pb-5 pl-8 text-start text-white">
+              <div>
+                <p className="pt-16 text-4xl fontBold mb-1">강의자료</p>
+              </div>
+            </div>
+            <div className="flex items-end justify-end h-full">
+              <img src={images.arrowM} alt="화살표" className="h-[14%] mb-6 mr-5 z-10" />
+            </div>
+          </div>
+        </button>
+      </div>
+
+      {/* 과제제출 */}
+      <div className="p-4">
+        <button
+          id={title}
+          onClick={() => navigate(`/cyberCampus/assignment/${title}`)}
+          className={`mx-auto relative group flex flex-col justify-between w-4/5 xl:w-4/5 h-[14rem] text-black rounded-md transition-colors duration-300 bg-[#FF7816]`}>
+          <div className="flex justify-between w-full h-full">
+            <div className="pb-5 pl-8 text-start text-white">
+              <div>
+                <p className="pt-16 text-4xl fontBold mb-1">과제제출</p>
+              </div>
+            </div>
+            <div className="flex items-end justify-end h-full">
+              <img src={images.arrowM} alt="화살표" className="h-[14%] mb-6 mr-5 z-10" />
+            </div>
+          </div>
+        </button>
+      </div>
+    </div>
+  </div>
+  </div>
   );
 };
