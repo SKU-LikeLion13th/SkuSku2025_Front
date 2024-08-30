b7cfd8c96245da4a766e623fe99ded435b7160d6
minseo121 <kittse1217@gmail.com>
2024-08-30 16:31:23 +0900

    fix:lectureboard 구조

--- src/pages/Admin/LectureManagement/LectureManagement.jsx
+++ src/pages/Admin/LectureManagement/LectureManagement.jsx
@@ -116,7 +116,7 @@ const LectureManagement = () => {
       <div className='w-full h-full flex flex-col items-center'>
         <div className="flex flex-col items-center justify-center pt-40 fontEB">
           <div className="text-[#3B79FF] my-2 ml-1 text-7xl">{track}</div>
-          <div className="mr-1 text-6xl">과제제출</div>
+          <div className="mr-1 text-6xl">강의자료</div>
         </div>
         <LectureLocation />
         {editingLecture ? (
