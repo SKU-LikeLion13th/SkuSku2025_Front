e33cbf8b79e5ddbf744ae8bbff1fde9db5883d09
이호연 <ihoyeon@ihoyeon-ui-MacBookAir.local>
2025-03-13 11:56:24 +0900

    [fix]Team, Project

--- src/pages/User/TeamIntro/Person.jsx
+++ src/pages/User/TeamIntro/Person.jsx
@@ -1,18 +1,21 @@
-import React from 'react';
-import Person0 from '../../../utils/Person.json';
+import React from "react";
+import Person0 from "../../../utils/Person.json";
 
-const Person = ({ idList, gapX = 'gap-x-8' }) => {
+const Person = ({ idList, gapX = "gap-x-8" }) => {
   return (
     <div className="flex justify-center px-4 mt-5 sm:px-0">
-      <div className={`grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 ${gapX} gap-y-12`}>
-        {idList.map(id => {
-          const member = Person0.find(member => member.id === id);
+      <div
+        className={`grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 ${gapX} gap-y-12`}
+      >
+        {idList.map((id) => {
+          const member = Person0.find((member) => member.id === id);
           if (!member) return null;
 
           return (
             <div
               key={member.id}
-              className="flex flex-col items-center justify-center w-full max-w-xs p-4 sm:w-48 bg-neutral-50 rounded-2xl sm:h-64">
+              className="flex flex-col items-center justify-center w-full max-w-xs p-4 sm:w-48 bg-neutral-50 rounded-2xl sm:h-64"
+            >
               <img
                 src={member.image}
                 alt={member.name}
@@ -20,13 +23,19 @@ const Person = ({ idList, gapX = 'gap-x-8' }) => {
               />
               <div className="flex items-center justify-center mt-4 whitespace-nowrap">
                 <img src={member.logo} alt="logo" className="w-6 h-6 mr-2" />
-                <span className="text-base text-black fontEB">{member.name}</span>
+                <span className="text-base text-black fontEB">
+                  {member.name}
+                </span>
               </div>
               <div className="text-center whitespace-nowrap">
-                <p className="pt-3 text-sm text-black fontBold">{member.major}</p>
+                <p className="pt-3 text-sm text-black fontBold">
+                  {member.major}
+                </p>
                 <p className="text-sm text-black fontBold">{member.role}</p>
                 <div className="w-20 h-[1.5px] bg-blue-500 mt-2 mx-auto sm:w-32" />
-                <p className="mt-2 text-sm text-blue-500 fontBold">{member.position}</p>
+                <p className="mt-2 text-sm text-blue-500 fontBold">
+                  {member.position}
+                </p>
               </div>
             </div>
           );
--- src/pages/User/TeamIntro/TeamIntro.jsx
+++ src/pages/User/TeamIntro/TeamIntro.jsx
@@ -1,23 +1,24 @@
-import React, { useEffect, useState } from 'react';
-import Person from './Person';
-import { useLocation } from 'react-router-dom';
+import React, { useEffect, useState } from "react";
+import Person from "./Person";
+import { useLocation } from "react-router-dom";
 
 const TeamIntro = () => {
   const location = useLocation();
-  const [activeTab, setActiveTab] = useState('tab12');
+  const [activeTab, setActiveTab] = useState("tab13");
 
-  const handleTabClick = tab => {
+  const handleTabClick = (tab) => {
     setActiveTab(tab);
   };
 
+  const id13gi = [19, 20, 21, 22, 23, 24, 25, 28, 26, 27]; // 13기
   const id12gi = [2, 6, 4, 5, 11, 7, 3, 8, 1, 9, 10]; // 12기
   const id11gi = [12, 13, 14, 16, 17, 15, 18]; // 11기
 
-  const tabClass = tab =>
+  const tabClass = (tab) =>
     `cursor-pointer text-xl fontBold relative ${
       activeTab === tab
-        ? 'text-white after:absolute after:left-0 after:bottom-[-2px] after:w-full after:h-[2px] after:bg-white'
-        : 'text-gray-500'
+        ? "text-white after:absolute after:left-0 after:bottom-[-2px] after:w-full after:h-[2px] after:bg-white"
+        : "text-gray-500"
     }`;
 
   return (
@@ -32,16 +33,35 @@ const TeamIntro = () => {
         </div>
 
         <div className="flex justify-center p-10 mt-10 space-x-20">
-          <div className={tabClass('tab12')} onClick={() => handleTabClick('tab12')}>
+          <div
+            className={tabClass("tab13")}
+            onClick={() => handleTabClick("tab13")}
+          >
+            13기
+          </div>
+          <div
+            className={tabClass("tab12")}
+            onClick={() => handleTabClick("tab12")}
+          >
             12기
           </div>
-          <div className={tabClass('tab11')} onClick={() => handleTabClick('tab11')}>
+          <div
+            className={tabClass("tab11")}
+            onClick={() => handleTabClick("tab11")}
+          >
             11기
           </div>
         </div>
 
-        {activeTab === 'tab12' && <Person idList={id12gi} gapX="gap-x-8 sm:gap-x-12" />}
-        {activeTab === 'tab11' && <Person idList={id11gi} gapX="gap-x-8 sm:gap-x-12" />}
+        {activeTab === "tab13" && (
+          <Person idList={id13gi} gapX="gap-x-8 sm:gap-x-12" />
+        )}
+        {activeTab === "tab12" && (
+          <Person idList={id12gi} gapX="gap-x-8 sm:gap-x-12" />
+        )}
+        {activeTab === "tab11" && (
+          <Person idList={id11gi} gapX="gap-x-8 sm:gap-x-12" />
+        )}
       </div>
     </>
   );
